fef32bc74950b7c387129b7e0d09bbff6595d5ce
19_Hà Huy Hiếu_0119 <hhhieu.dhkl17a2hn@sv.uneti.edu.vn>
2024-03-28 21:43:01 +0700

    first commit

---
+++ lab3/bai tap/lab4.py
@@ -0,0 +1,18 @@
+n=int(input("nhập vào số hàng: "))
+# nửa hình trên
+for i in range(n ): 
+    for j in range(n-i-1):
+        print(' ',end="") 
+    for j in range(i+1):
+        print("*",end='')
+    print()
+
+##################################
+    n=int(input("nhập vào số hàng: "))
+# nửa hình trên
+for i in range(n ): 
+    for j in range(n-i-1):
+        print(' ',end="") 
+    for j in range(i+1):
+        print("*",end='')
+    print()    
